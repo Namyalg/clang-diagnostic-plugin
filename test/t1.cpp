71a3e2dd72a7dc0e859d7e03bcbc51bbaaea5f9a
Namya LG <namyalg@gmail.com>
2022-07-28 04:52:45 +0530

    chore:test case for declaration, array type

---
+++ test/t1.cpp
@@ -0,0 +1,16 @@
+#include<cstdlib>
+
+int main() {
+
+	// -------------- POINTER TO AN ARRAY - Type : Declaration ------------------------
+	int* ptr = (int *)malloc(4);
+
+	ptr[0] = 2;
+	
+	// if(ptr != nullptr){
+	// 	for (int i = 0; i < 5; i++) {
+    // 		ptr[i] = i * 2;
+  	// 	}	
+	// }
+	return 0;
+}
